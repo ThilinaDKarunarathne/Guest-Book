a620e5b5906d1041393a000d7934ae7a6d5c1b17
Thilina Dhanajaya <adamsampath2002@gmail.com>
2024-12-26 21:30:33 +0530

    form created

--- guest_book_client/src/index.js
+++ guest_book_client/src/index.js
@@ -17,7 +17,29 @@ const App = () => {
         <p className='centerText'>No Posts</p>
       </div>
       <div className='modal'>
-        
+        <div className='form'>
+          <div className='form-header'>
+            <div>
+              <p className='form-header-text'>Create a Note</p>
+            </div>
+            <div>
+              <a href='#' className='close-btn'>X</a>
+            </div>
+          </div>
+          <form action="">
+            <div className='form-group'>
+              <label htmlFor='title'>Title</label>
+              <input type='text' name='' id='title' className='form-control' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='content'>Content</label>
+              <textarea className='form-control' name='content' id='title' cols='30' rows='8'></textarea>
+            </div>
+            <div className='form-group'>
+              <input type='submit' value="SAVE" className='btn' />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
